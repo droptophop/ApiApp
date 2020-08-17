a7de1e1893423f6d4e9689261b5274712a50b395
Harold Hopson <hdhopson88@yahoo.com>
2020-08-17 12:18:59 -0700

    added console app to consume api app

---
+++ ConsoleApp/ConsoleApp.Client/Class1.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp.Client
+{
+    public class Class1
+    {
+        public string date { get; set; }
+
+        public int temperatureF { get; set; }
+
+        public string summary { get; set; }
+    }
+}
---
+++ ConsoleApp/ConsoleApp.Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp.Client
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("https://weatherappapi.azurewebsites.net");
+
+            var val = "application/json";
+            var media = new MediaTypeWithQualityHeaderValue(val);
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(media);
+
+            try
+            {
+                var weatherSummaries = new Class1();
+
+                // read
+                System.Console.WriteLine("Weather Summaries: ");
+                List<Class1> weatherItems = GetWeatherItems();
+                for (int i = 0; i < weatherItems.Count; i++)
+                {
+                    var oneSummary = weatherItems[i];
+                    System.Console.WriteLine($"{i + 1}: {oneSummary.temperatureF} Degrees Farenheit, {oneSummary.summary}\n{oneSummary.date}\n");
+                }
+            }
+            catch (System.Exception e)
+            {
+                
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
+        private static List<Class1> GetWeatherItems()
+        {
+            var action = $"/weatherforecast";
+            var request = client.GetAsync(action);
+            var response = request.Result.Content.ReadAsAsync<List<Class1>>();
+
+            return response.Result;
+        }
+    }
+}
